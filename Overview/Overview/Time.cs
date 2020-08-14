9833a9d70665a14bc3eb078e33806d7f54cc8761
thelittlecitizen13 <amitassa9@gmail.com>
2020-08-14 19:39:53 +0300

    + - operators functions added

--- Overview/Overview/Time.cs
+++ Overview/Overview/Time.cs
@@ -37,7 +37,60 @@ namespace Overview
                 else
                     milisecShow = "0" + _miliseconds;
             
-            Console.WriteLine($"Time is: {hourShow}.{minShow}.{secShow}.{milisecShow}");
+            Console.WriteLine($"Time is: {hourShow}:{minShow}:{secShow}.{milisecShow}");
+        }
+
+        public static Time operator+ (Time timeA, Time timeB)
+        {
+            int minCombined = timeA._minutes + timeB._minutes;
+            int secCombined = timeA._seconds + timeB._seconds; ;
+            int milisecCombined = timeA._miliseconds + timeA._miliseconds;
+            int hoursCombined = timeA._hours + timeB._hours;
+
+            if (milisecCombined > 1000)
+            {
+                milisecCombined -= 1000;
+                secCombined++;
+            }
+            if (secCombined > 60)
+            {
+                secCombined -= 60;
+                minCombined++;
+            }
+            if (minCombined > 60)
+            {
+                minCombined -= 60;
+                hoursCombined++;
+            }
+            return new Time(hoursCombined, minCombined, secCombined, milisecCombined);
+        }
+        public static Time operator -(Time timeA, Time timeB)
+        {
+            int minCombined = timeA._minutes - timeB._minutes;
+            int secCombined = timeA._seconds - timeB._seconds; ;
+            int milisecCombined = timeA._miliseconds - timeA._miliseconds;
+            int hoursCombined = timeA._hours - timeB._hours;
+
+            if (milisecCombined < 0)
+            {
+                milisecCombined += 1000;
+                secCombined--;
+            }
+            if (secCombined < 0)
+            {
+                secCombined += 60;
+                minCombined--;
+            }
+            if (minCombined < 0)
+            {
+                minCombined += 60;
+                hoursCombined--;
+            }
+            if (hoursCombined < 0)
+            {
+                throw new ArithmeticException("The substracted time is lower than the substracting time. Cannot create a negative Time!");
+            }
+            return new Time(hoursCombined, minCombined, secCombined, milisecCombined);
         }
     }
 }
