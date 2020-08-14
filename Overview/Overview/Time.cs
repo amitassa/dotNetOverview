7a96119d3b5549381149903e79398830a558982c
thelittlecitizen13 <amitassa9@gmail.com>
2020-08-14 19:13:12 +0300

    ShowTime function added

--- Overview/Overview/Time.cs
+++ Overview/Overview/Time.cs
@@ -18,5 +18,26 @@ namespace Overview
             _minutes = min;
             _seconds = sec;
         }
+
+        public void ShowTime()
+        {
+            string hourShow = _hours.ToString();
+            string minShow = _minutes.ToString();
+            string secShow = _seconds.ToString();
+            string milisecShow = _miliseconds.ToString();
+            if (_hours < 10)
+                hourShow = "0" + _hours;
+            if (_minutes < 10)
+                minShow = "0" + _minutes;
+            if (_seconds < 10)
+                secShow = "0" + _seconds;
+            if (_miliseconds < 100)
+                if (_miliseconds < 10)
+                    milisecShow = "00" + _miliseconds;
+                else
+                    milisecShow = "0" + _miliseconds;
+            
+            Console.WriteLine($"{}");
+        }
     }
 }
