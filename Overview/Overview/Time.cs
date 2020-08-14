0ed6935f3d37e6109ee88c4f96625d7f9d831211
thelittlecitizen13 <amitassa9@gmail.com>
2020-08-14 18:55:01 +0300

    Fields and ctor added

--- Overview/Overview/Time.cs
+++ Overview/Overview/Time.cs
@@ -6,5 +6,17 @@ namespace Overview
 {
     class Time
     {
+        private int _hours;
+        private int _minutes;
+        private int _seconds;
+        private int _miliseconds;
+
+        public Time (int hours, int min, int sec, int milisec)
+        {
+            _hours = hours;
+            _miliseconds = milisec;
+            _minutes = min;
+            _seconds = sec;
+        }
     }
 }
